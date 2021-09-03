8462c6a713a3effbc40428a522d68eb148cc864b
JurinKI <Jukrin.sa@mail.wu.ac.th>
2021-09-03 13:53:14 +0700

    Edit file

--- src/TotalsaralySend.java
+++ src/TotalsaralySend.java
@@ -2,9 +2,53 @@ import java.util.Scanner;
 public class TotalsaralySend {
 
 	public static void main(String[] args) {
+		TotalsaralyTest t [][] = new TotalsaralyTest[2][3];
 		Scanner in = new Scanner(System.in);
-        System.out.print("What is your name : ");
-        String name = in.nextLine();
+		String fname,lname;
+		int i = 0 ,j = 0, time; 
+		char yn = 'y';
+		
+		for(i=0; i< t.length ; i++) {
+			
+			for(j=0; j < 3 ; j++) {
+				switch (j) {
+				case 0:
+					System.out.print("Enter your First name : ");
+			        fname = in.nextLine();
+			        t[i][j].setFname(fname);
+					break;
+				case 1:
+					System.out.print("Enter your Last name : ");
+			        lname = in.nextLine();
+			        t[i][j].setLname(lname);
+					break;
+				case 2:
+					System.out.print("How much hours : ");
+					time = in.nextInt();
+			        t[i][j].setTime(time);
+					break;
+				}
+			}
+			
+			System.out.println("Col"+i);
+		}
+		
+//			System.out.print("Enter your First name : ");
+//	        fname = in.nextLine();
+//	        System.out.print("Enter your Last name : ");
+//	        lname = in.nextLine();
+//	        System.out.print("How much hours : ");
+//	        time = in.nextInt();
+//	        t.TotalsaralyTest( fname, lname,time);
+//	        System.out.print("Do you want to add (Y/N) : ");
+//	        yn = in.next().charAt(0);
+//	        i= i+1;
+//		
+		
+		
+		
+		
+        
 	}
 
 }
--- src/TotalsaralyTest.java
+++ src/TotalsaralyTest.java
@@ -1,4 +1,58 @@
 
 public class TotalsaralyTest {
+	private String fname;
+	private String lname;
+	private int time;
+	
+	
+	public void TotalsaralyTest(String fname , String lname,int time ) {
+        this.fname = fname;
+        this.lname = lname;
+		this.time = time;
+    }
+	
+	
+	
+	public String getFname() {
+		return fname;
+	}
 
+
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+
+
+	public String getLname() {
+		return lname;
+	}
+
+
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+
+
+	public int getTime() {
+		return time;
+	}
+
+
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+
+
+	public int salary() {
+		int sum = 0;
+		sum = time*500;
+		return sum;
+	}
+	
 }
