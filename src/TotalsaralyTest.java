f0d536f88804d26874b09373944e21e7b7669194
Tanadon62115639 <sonicxdk007@hotmail.co.uk>
2021-09-11 01:01:34 +0700

    Update total Salary Test

--- src/TotalsaralyTest.java
+++ src/TotalsaralyTest.java
@@ -1,47 +1,68 @@
-
 public class TotalsaralyTest extends TotalsaralySend{
-	private String idmanager;
-	private String name;
-	private int salary;
-	private int sales;
-	
-	public void TotalsaralyTest (String idmanager , String name,int salary ,int sales) {
+    private String idmanager;
+    private String name;
+    private int salary;
+    private double sales;
+
+    public TotalsaralyTest ( ) {
+
+    }
+
+    public  TotalsaralyTest (String idmanager , String name,int salary ) {
         this.idmanager = idmanager;
         this.name = name;
-		this.salary = salary;
-		this.sales = sales;
+        this.salary = salary;
     }
-	
-	
-	
-	public String getidmanager() {
-		return this.idmanager;
-	}
 
 
-	public String getname() {
-		return this.name;
-	}
+    public String getidmanager() {
+        return this.idmanager;
+    }
 
 
-	public int getsalary() {
-		return this.salary;
-	}
-	
-	public int getsales() {
-		return this.sales;
-	}
+    public String getname() {
+        return this.name;
+    }
 
-	public double salary() {
-		double sum = 0;
-		if(sales<=50000) {
-			sum = salary+(salary*0.05);
-		}else
-			sum = salary+(salary*0.10);
-		if(sum<12000) {
-			sum = 12000;
-		}
-		return sum;
-	}
-	
+
+    public int getsalary() {
+        return this.salary;
+    }
+
+    public double getsales() {
+        return this.sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+
+
+    public double salary() {
+        double sum = 0;
+        if(salary<=10000)
+        {
+            if(sales<=50000) {
+            sum = salary+(salary*0.05);
+        }else
+            sum = salary+(salary*0.10);
+        }
+
+        else {
+            if(sales<=100000) {
+                sum = salary+(salary*0.05);
+            }else
+                sum = salary+(salary*0.10);
+            if(sum<12000) {
+                sum=12000;
+            }
+        }
+        return sum;
+    }
+
+    public String toString() {
+        return "ManagerID :  " + this.idmanager + "   ManagerName :   " + this.name + "   Salary :   " + this.salary;
+    }
+
 }
